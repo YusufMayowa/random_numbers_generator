f37dd7a26404909544922603f91ad2413107613d
Abubakar <yusuf.mayowa.a@gmail.com>
2019-04-09 22:18:25 +0100

    another commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random randomNumberGenerator = new Random();
+
+            for (int i = 0; i < 40; i++)
+            {
+                string uniqueDigit = string.Concat(
+                    randomNumberGenerator.Next(0, 9),
+                    randomNumberGenerator.Next(0, 9),
+                    randomNumberGenerator.Next(0, 9),
+                    randomNumberGenerator.Next(0, 9)
+                    );
+
+                Console.WriteLine(uniqueDigit);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
